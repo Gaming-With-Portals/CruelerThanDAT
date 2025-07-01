9053eab9a671142a45d316bd17f7cef160744b29
Gaming-With-Portals <73725114+Gaming-With-Portals@users.noreply.github.com>
2025-07-01 14:45:19 -0600

    whoops forgot to remove astc

--- CruelerThanDAT/inc/TextureHelper.h
+++ CruelerThanDAT/inc/TextureHelper.h
@@ -3,7 +3,6 @@
 #define GL_COMPRESSED_RGBA_S3TC_DXT3_EXT 0x83F2
 #define GL_COMPRESSED_RGBA_S3TC_DXT5_EXT 0x83F3
 #include "BinaryHandler.h"
-#include "astcenc.h"
 
 enum WTB_EXDATA {
 	EXDATA_XBOX,
